1207314f14f28bdf83a6f072a627dc5f67b36b87
Devendra Rokade <drkade981@gmail.com>
2024-06-09 11:54:17 +0530

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-
+import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>E-Dashboard</h1>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/products" element= {<h1>Product listing component</h1>} />
+        <Route path="/add-product" element= {<h1>Product add component</h1>} />
+        <Route path="/profile" element= {<h1>Profile component</h1>} />
+        <Route path="/logout" element= {<h1>Logout component</h1>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
