10d6557ffc6df2ea21b4ee9ac6c69c141d311857
Devendra Rokade <drkade981@gmail.com>
2024-07-15 00:55:52 +0530

    manage signup link and add data in local storage and manage use loggedin

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+    const auth = localStorage.getItem('user');
     return (
         <div>
             <ul className="nav-ul">
                 <li><Link to="/products">Product</Link></li>
                 <li><Link to="/add-product">Add Product</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li><Link to="/logout">Logout</Link></li>
+                <li>{auth ? <Link to="/logout">Logout</Link> : <Link to="/register">Signup</Link>} </li>
             </ul>
         </div>
     )
---
+++ frontend/src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiBaseUrl: 'http://localhost:5000', // Replace with your actual API base URL
+    // Add other configurations here
+  };
+  
+  export default config;
